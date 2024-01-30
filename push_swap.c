8744256e82519c121bf461d1c3697e5e6bdddcf4
dudamakesthings <dudamakesthings@gmail.com>
2024-01-30 16:53:25 +0100

    Add part of the input_check

--- push_swap.c
+++ push_swap.c
@@ -6,18 +6,75 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/02 15:08:35 by edribeir      #+#    #+#                 */
-/*   Updated: 2024/01/26 11:26:42 by edribeir      ########   odam.nl         */
+/*   Updated: 2024/01/30 16:52:31 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void ft_reverse(t_stack **head)
+void	input_verification(char **arguments)
+{
+	int	i;
+
+	i = 0;
+	while (arguments[i])
+	{
+		ft_printf("%s\n", arguments[i]);
+		// if(ft_isdigit(*arguments[i]) == 1)
+		// {
+		// 	ft_printf("%s\n", "ERROR, Wrong Input");
+		// 	// return (-1);
+		// }
+		i++;
+	}
+	// return (1);
+}
+
+t_stack	*string_to_stack(t_stack *stk_a, char **argv)
+{
+	int		i;
+	char **array;
+	int nb;
+	
+	
+	array = ft_split(*argv, ' ');
+	i = 0;
+	while(array[i])
+	{
+		// if (input_verification(&array[i]) == -1)
+		// 	break ;
+		nb = ft_atoi(array[i]);
+		if (stk_a == NULL)
+			stk_a = new_node(ft_atoi(array[i]));
+		else
+			stk_a = add_to_stack(stk_a, nb);
+		i++;
+	}
+	free(array);
+	return (stk_a);
+	// i = 0;
+	// if (argc == 2)
+	// 	argv = ft_split(argv[1], ' ');
+	// else
+	// 	i = 1;
+	// while (i < argc)
+	// {
+	// 	tmp = atoi(argv[i]);
+	// 	if(ft_isdigit(tmp) == 1)
+	// 	{
+	// 		ft_printf("%s\n", "ERROR");
+	// 		return ;
+	// 	}
+	// 	i++;
+	// }
+}
+
+void	ft_reverse(t_stack **head)
 {
 	t_stack *new_tail;
 	t_stack *temp;
 
-	if(*head == NULL)
+	if (*head == NULL)
 		return ;
 	temp = *head;
 	while (temp->next != NULL)
@@ -29,12 +86,12 @@ void ft_reverse(t_stack **head)
 	new_tail->next = NULL;
 	*head = temp;
 }
-void ft_rotate(t_stack **head)
+void	ft_rotate(t_stack **head)
 {
 	t_stack *first;
 	t_stack *temp;
 
-	if(*head == NULL)
+	if (*head == NULL)
 		return ;
 	first = *head;
 	temp = *head;
@@ -44,7 +101,7 @@ void ft_rotate(t_stack **head)
 	temp->next = first;
 	first->next = NULL;
 }
-void ft_push(t_stack **stack_from, t_stack **stack_to)
+void	ft_push(t_stack **stack_from, t_stack **stack_to)
 {
 	t_stack *temp;
 
@@ -61,7 +118,7 @@ void ft_push(t_stack **stack_from, t_stack **stack_to)
 		*stack_to = temp;
 	}
 }
-void ft_swap(t_stack **head)
+void	ft_swap(t_stack **head)
 {
 	t_stack *temp;
 
@@ -72,7 +129,7 @@ void ft_swap(t_stack **head)
 	temp->next = *head;
 	*head = temp;
 }
-void print_numbers(t_stack *stack)
+void	print_numbers(t_stack *stack)
 {
 	t_stack *temp;
 
@@ -82,49 +139,56 @@ void print_numbers(t_stack *stack)
 		ft_printf("%d\n", temp->number);
 		temp = temp->next;
 	}
+	// need to free allocation for temp?
 }
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	int i;
 
 	i = 1;
 	if (argc < 2)
 		return (0);
-// checar input;
 	t_stack *st_a;
 	t_stack *st_b;
 
 	st_a = NULL;
 	st_b = NULL;
-	if (st_a == NULL)
-		st_a = new_node(ft_atoi(argv[i]));
-	// if (st_b == NULL)
-	// 	st_b = new_node(ft_atoi(argv[i]));
-	i++;
-	while (i < argc)
+	if (argc == 2)
+		st_a = string_to_stack(st_a, &argv[1]);
+	else if (argc > 2)
 	{
-		st_a = add_to_stack(st_a, ft_atoi(argv[i]));
+		if (st_a == NULL)
+			st_a = new_node(ft_atoi(argv[i]));
+		// if (st_b == NULL)
+		// 	st_b = new_node(ft_atoi(argv[i]));
 		i++;
+		while (i < argc)
+		{
+			input_verification(&argv[i]);
+			st_a = add_to_stack(st_a, ft_atoi(argv[i]));
+			i++;
+		}
 	}
 /////////////////////////////////////////////////////////////
+	ft_printf("%s\n", "---");
 	print_numbers(st_a);
 	ft_printf("%s\n", "---");
-	ft_rotate(&st_a);
-	print_numbers(st_a);
-	ft_printf("%s\n", "---");
-	ft_reverse(&st_a);
-	print_numbers(st_a);
-	ft_printf("%s\n", "---");
-	ft_swap(&st_a);
-	print_numbers(st_a);
-	ft_push(&st_a, &st_b);
-	ft_printf("%s\n", "--- stack b");
-	print_numbers(st_b);
-	ft_printf("%s\n", "--- stack a");
-	print_numbers(st_a);
-	ft_push(&st_a, &st_b);
-	ft_printf("%s\n", "--- stack b");
-	print_numbers(st_b);
-	ft_printf("%s\n", "--- stack a");
-	print_numbers(st_a);
+	// ft_rotate(&st_a);
+	// print_numbers(st_a);
+	// ft_printf("%s\n", "---");
+	// ft_reverse(&st_a);
+	// print_numbers(st_a);
+	// ft_printf("%s\n", "---");
+	// ft_swap(&st_a);
+	// print_numbers(st_a);
+	// ft_push(&st_a, &st_b);
+	// ft_printf("%s\n", "--- stack b");
+	// print_numbers(st_b);
+	// ft_printf("%s\n", "--- stack a");
+	// print_numbers(st_a);
+	// ft_push(&st_a, &st_b);
+	// ft_printf("%s\n", "--- stack b");
+	// print_numbers(st_b);
+	// ft_printf("%s\n", "--- stack a");
+	// print_numbers(st_a);
 }
