14995ec728c1958e7d12560c00f173b6fa5c2590
dudamakesthings <dudamakesthings@gmail.com>
2024-02-01 17:56:02 +0100

    delete -o files

--- Libft/ft_lstadd_back.c
+++ Libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/03 10:04:49 by edribeir      #+#    #+#                 */
-/*   Updated: 2024/01/31 16:09:54 by edribeir      ########   odam.nl         */
+/*   Updated: 2024/02/01 17:54:53 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,3 @@ void	ft_lstadd_back(t_list **lst_head, t_list *new_node)
 		return ;
 	}
 }
-1 ->nxt 2 ->nxt NULL
